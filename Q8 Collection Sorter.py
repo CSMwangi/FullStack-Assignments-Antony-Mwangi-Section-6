d41889621797fa93db0750ea7386d8b48570507c
Engineer Mwangi <info.csmwangi@gmail.com>
2025-11-07 17:40:49 +0300

    Update Q8 Collection Sorter.py

--- Q8 Collection Sorter.py
+++ Q8 Collection Sorter.py
@@ -1,3 +1,7 @@
+# 8 COLLECTION SORTER
+# This program loops through a list of numbers and prints only those greater than 10.
+# It also counts how many numbers meet that condition using a loop and continue statement.
+
 numbers = [4, 15, 23, 9, 12, 7, 18]
 count = 0
 
@@ -8,3 +12,4 @@ for n in numbers:
     count += 1
 
 print(f"Total numbers greater than 10: {count}")
+
