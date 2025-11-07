70c32970eb17f07abe63096db30ea99d079223d4
Engineer Mwangi <info.csmwangi@gmail.com>
2025-11-07 17:35:17 +0300

    Update Q15 Nested Data Lookup.py

--- Q15 Nested Data Lookup.py
+++ Q15 Nested Data Lookup.py
@@ -1,3 +1,7 @@
+# 15 NESTED DATA LOOKUP
+# This program retrieves a family member’s details (name and birth year) from a nested dictionary.
+# It practices nested data access and condition checking.
+
 myFamily = {
     "father": {"name": "Antony Mwangi", "year": 2002},
     "mother": {"name": "Naomi Wangui", "year": 2005}
@@ -11,3 +15,4 @@ else:
     print("Family member not found.")
 
     
+
