931b150bcbf71fcf91b6bef98215a2efb2b86603
Engineer Mwangi <info.csmwangi@gmail.com>
2025-11-07 17:32:30 +0300

    Update Q10 Membership Checker.py

--- Q10 Membership Checker.py
+++ Q10 Membership Checker.py
@@ -1,3 +1,7 @@
+# 10 MEMBERSHIP CHECKER
+# This program checks if a given car type exists in a list.
+# It uses the 'in' operator to test membership in a collection.
+
 cars = ["jeep", "suv", "sedan", "probox", "cx5","demio"]
 car_name = input("Enter car name: ").lower()
 
@@ -7,3 +11,4 @@ else:
     print("Sorry, not available.")
 
     
+
