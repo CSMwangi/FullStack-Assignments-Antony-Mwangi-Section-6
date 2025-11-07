1cc7ccafe7b5f21ca1194521b9edd920da671316
Engineer Mwangi <info.csmwangi@gmail.com>
2025-11-07 17:39:25 +0300

    Update Q6 Login Simulation.py

--- Q6 Login Simulation.py
+++ Q6 Login Simulation.py
@@ -1,3 +1,8 @@
+# 6 LOGIN SIMULATION
+# This program simulates a basic login system.
+# It checks if the entered email and password match stored credentials.
+
+
 stored_email = "antony@gmail.com"
 stored_password = "12345"
 
@@ -8,3 +13,4 @@ if email == stored_email and password == stored_password:
     print("Login successful!")
 else:
     print("Invalid credentials!")
+
