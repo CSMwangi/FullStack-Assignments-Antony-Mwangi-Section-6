a26975c70732c25b4effda7d5757706cfe7d48e4
Engineer Mwangi <info.csmwangi@gmail.com>
2025-11-07 17:38:09 +0300

    Update Q4 Grade Classifier.py

--- Q4 Grade Classifier.py
+++ Q4 Grade Classifier.py
@@ -1,3 +1,7 @@
+# 4 GRADE CLASSIFIER
+# This program classifies a student’s test score into Excellent, Good, or Needs Improvement.
+# It uses if-elif-else statements and validates the score range.
+
 score = int(input("Enter your test score (0–100): "))
 
 if 80 <= score <= 100:
@@ -8,3 +12,4 @@ elif 0 <= score < 50:
     print("Needs Improvement")
 else:
     print("Invalid score entered.")
+
